0b1452c7973ff2897f3265df4cc50782f8df5157
retagg <retagg@mailcatch.com>
2014-06-19 20:27:13 -0700

    Create Constants.java

---
+++ org/parabot/reta/dhkiller/data/Constants.java
@@ -0,0 +1,6 @@
+package org.parabot.reta.dhkiller.data;
+
+public class Constants {
+        private final int[] DHAROK_ITEM_IDS = {4717, 4719, 4721, 4723, 995, 1631};
+        private final Tile START_LOCATION = new Tile(3575, 3298);
+}
